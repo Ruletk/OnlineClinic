6e501d95bf35b9e9321f35ba0cb651afc520b41f
Ruletk <damir.timergazin@gmail.com>
2025-05-17 13:09:39 +0500

    feat(auth): Refactor publisher to use interface.

--- apps/auth/internal/nats/publisher.go
+++ apps/auth/internal/nats/publisher.go
@@ -7,15 +7,19 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type Publisher struct {
+type Publisher interface {
+	PublishEmailMessage(to, subject, message string) error
+}
+
+type NatsPublisher struct {
 	nc *nats.Conn
 }
 
-func NewPublisher(nc *nats.Conn) *Publisher {
-	return &Publisher{nc: nc}
+func NewPublisher(nc *nats.Conn) Publisher {
+	return &NatsPublisher{nc: nc}
 }
 
-func (p *Publisher) PublishEmailMessage(to, subject, message string) error {
+func (p *NatsPublisher) PublishEmailMessage(to, subject, message string) error {
 	logging.Logger.Debug("Publishing email message to NATS")
 	data, err := proto.Marshal(&email.SendEmailRequest{
 		To:      to,
