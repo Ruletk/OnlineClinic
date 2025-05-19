57b06a7bc123b9ce46b74d7aab65e41e17cff540
Ruletk <damir.timergazin@gmail.com>
2025-05-19 10:25:10 +0500

    refactor: Added graceful shutdown for empty services.

--- apps/prescription/cmd/main.go
+++ apps/prescription/cmd/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	mainCtx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(mainCtx, 5*time.Second)
+	defer shutdownCancel()
+
+	g, gCtx := errgroup.WithContext(shutdownCtx)
+
+	// Main goroutine
+	g.Go(func() error {
+		for {
+			select {
+			case <-gCtx.Done():
+				fmt.Println("Main goroutine shutting down...")
+				return nil
+			default:
+				// Simulate some work
+				fmt.Println("Main goroutine doing work...")
+				time.Sleep(1 * time.Second)
+			}
+		}
+	})
+
+	// Shutdown goroutines
+	g.Go(func() error {
+		// Simulate some work
+		fmt.Println("Doing some work...")
+		// Wait for the context to be done
+		<-gCtx.Done()
+		fmt.Println("Shutting down...")
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("All done!")
 }
--- apps/user/cmd/main.go
+++ apps/user/cmd/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	mainCtx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	g, gCtx := errgroup.WithContext(mainCtx)
+
+	// Main goroutine
+	g.Go(func() error {
+		fmt.Println("Hello World!")
+		return nil
+	})
+
+	// Shutdown goroutines
+	g.Go(func() error {
+		// Simulate some work
+		fmt.Println("Doing some work...")
+		// Wait for the context to be done
+		<-gCtx.Done()
+		fmt.Println("Shutting down...")
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("All done!")
 }
