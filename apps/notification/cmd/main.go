065897c7fc35c776072600c0f697cd539a1fae61
Ruletk <damir.timergazin@gmail.com>
2025-05-19 12:46:28 +0500

    refactor(notification): Added graceful shutdown.

--- apps/notification/cmd/main.go
+++ apps/notification/cmd/main.go
@@ -1,40 +1,69 @@
 package main
 
 import (
-	"fmt"
-	config2 "github.com/Ruletk/OnlineClinic/pkg/config"
-	"github.com/Ruletk/OnlineClinic/pkg/logging"
-	"github.com/nats-io/nats.go"
-	"notification/internal/repositories/email"
-	"notification/internal/subscribers"
+    "context"
+    "fmt"
+    "github.com/Ruletk/OnlineClinic/pkg/config"
+    "github.com/Ruletk/OnlineClinic/pkg/logging"
+    "github.com/nats-io/nats.go"
+    "golang.org/x/sync/errgroup"
+    "notification/internal/repositories/email"
+    "notification/internal/subscribers"
+    "os/signal"
+    "syscall"
+    "time"
 )
 
 func main() {
-	config, err := config2.GetDefaultConfiguration()
-	if err != nil {
-		fmt.Printf("Failed to load configuration: %v\n", err)
-		panic(err)
-	}
-	logging.InitLogger(*config)
-	logging.Logger.Infof("Starting Notification Service")
+    mainCtx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+    defer cancel()
 
-	emailSender := email.NewMockEmailSender()
-	logging.Logger.Debugf("Creating NATS connection: %s", config.Nats.Url)
-	conn, err := nats.Connect(config.Nats.Url, nats.Name("Notification Service"))
-	if err != nil {
-		logging.Logger.WithError(err).Error("Failed to connect to NATS")
-		panic(err)
-	}
-	defer conn.Close()
+    cfg, err := config.GetDefaultConfiguration()
+    if err != nil {
+        fmt.Printf("Failed to load configuration: %v\n", err)
+        panic(err)
+    }
+    logging.InitLogger(*cfg)
+    logging.Logger.Infof("Starting Notification Service")
 
-	natsService := subscribers.NewNatsService(conn, emailSender)
-	natsService.InitNatsSubscriber()
+    emailSender := email.NewMockEmailSender()
 
-	logging.Logger.Info("NATS subscriber initialized and running")
+    logging.Logger.Debugf("Creating NATS connection: %s", cfg.Nats.Url)
+    conn, err := nats.Connect(cfg.Nats.Url, nats.Name("Notification Service"))
+    if err != nil {
+        logging.Logger.WithError(err).Error("Failed to connect to NATS")
+        panic(err)
+    }
+    defer conn.Close()
 
-	// Просто ждем сигнала завершения
-	wait := make(chan struct{})
-	<-wait
+    natsService := subscribers.NewNatsService(conn, emailSender)
 
-	logging.Logger.Info("Service stopped")
+    logging.Logger.Info("NATS subscriber initialized and running")
+
+    shutdownCtx, shutdownCancel := context.WithTimeout(mainCtx, 5*time.Second)
+    defer shutdownCancel()
+
+    g, gCtx := errgroup.WithContext(shutdownCtx)
+
+    // Main goroutine
+    g.Go(func() error {
+        return natsService.InitNatsSubscriber(gCtx)
+    })
+
+    // Shutdown goroutines
+    g.Go(func() error {
+        // Simulate some work
+        fmt.Println("Doing some work...")
+        // Wait for the context to be done
+        <-gCtx.Done()
+        fmt.Println("Shutting down...")
+        return nil
+    })
+
+    if err := g.Wait(); err != nil {
+        logging.Logger.WithError(err).Error("Error during shutdown")
+    } else {
+        logging.Logger.Info("Service stopped successfully")
+    }
+    logging.Logger.Info("Service stopped")
 }
--- apps/notification/internal/subscribers/nats.go
+++ apps/notification/internal/subscribers/nats.go
@@ -1,54 +1,62 @@
 package subscribers
 
 import (
-	"github.com/Ruletk/OnlineClinic/pkg/logging"
-	natspb "github.com/Ruletk/OnlineClinic/pkg/proto/nats/gen"
-	"github.com/nats-io/nats.go"
-	"google.golang.org/protobuf/proto"
-	"notification/internal/models"
-	"notification/internal/repositories/email"
+    "context"
+    "github.com/Ruletk/OnlineClinic/pkg/logging"
+    natspb "github.com/Ruletk/OnlineClinic/pkg/proto/nats/gen"
+    "github.com/nats-io/nats.go"
+    "google.golang.org/protobuf/proto"
+    "notification/internal/models"
+    "notification/internal/repositories/email"
 )
 
 // NatsService is a struct that represents the NATS service.
 type NatsService struct {
-	emailSender email.Sender
-	client      *nats.Conn
+    emailSender email.Sender
+    client      *nats.Conn
 }
 
 // NewNatsService creates a new NatsService instance.
 func NewNatsService(client *nats.Conn, emailSender email.Sender) *NatsService {
-	return &NatsService{
-		client:      client,
-		emailSender: emailSender,
-	}
+    return &NatsService{
+        client:      client,
+        emailSender: emailSender,
+    }
 }
 
-func (n *NatsService) InitNatsSubscriber() {
-	_, err := n.client.Subscribe("email.message", n.emailSubscriber)
-	if err != nil {
-		logging.Logger.Error("Failed to subscribe to NATS topic: %v", err)
-		return
-	}
-	logging.Logger.Debug("NATS subscriber initialized successfully")
-	logging.Logger.Info("NATS subscriber initialized successfully")
+func (n *NatsService) InitNatsSubscriber(ctx context.Context) error {
+    sub, err := n.client.Subscribe("email.message", n.emailSubscriber)
+    if err != nil {
+        logging.Logger.Error("Failed to subscribe to NATS topic: %v", err)
+        return err
+    }
+    defer func(sub *nats.Subscription) {
+        _ = sub.Unsubscribe()
+    }(sub)
+
+    logging.Logger.Info("NATS subscriber initialized successfully")
+
+    <-ctx.Done()
+    logging.Logger.Debug("NATS subscriber received shutdown signal")
+    return nil
 }
 
 func (n *NatsService) emailSubscriber(msg *nats.Msg) {
-	logging.Logger.Debug("Received email message request")
-	var natsEmailMessage natspb.EmailMessage
-	if err := proto.Unmarshal(msg.Data, &natsEmailMessage); err != nil {
-		logging.Logger.Error("Failed to unmarshal email message: %v", err)
-		return
-	}
-	logging.Logger.Debugf("Unmarshalled email message, to: %s", natsEmailMessage.To[0:10])
-	emailMsg := models.EmailMessage{
-		To:      natsEmailMessage.To,
-		Subject: natsEmailMessage.Subject,
-		Message: natsEmailMessage.Message,
-	}
-	if err := n.emailSender.Send(emailMsg); err != nil {
-		logging.Logger.Error("Failed to send email: %v", err)
-		return
-	}
-	logging.Logger.Infof("Email sent successfully to: %s", natsEmailMessage.To[0:10])
+    logging.Logger.Debug("Received email message request")
+    var natsEmailMessage natspb.EmailMessage
+    if err := proto.Unmarshal(msg.Data, &natsEmailMessage); err != nil {
+        logging.Logger.Error("Failed to unmarshal email message: %v", err)
+        return
+    }
+    logging.Logger.Debugf("Unmarshalled email message, to: %s", natsEmailMessage.To[0:10])
+    emailMsg := models.EmailMessage{
+        To:      natsEmailMessage.To,
+        Subject: natsEmailMessage.Subject,
+        Message: natsEmailMessage.Message,
+    }
+    if err := n.emailSender.Send(emailMsg); err != nil {
+        logging.Logger.Error("Failed to send email: %v", err)
+        return
+    }
+    logging.Logger.Infof("Email sent successfully to: %s", natsEmailMessage.To[0:10])
 }
