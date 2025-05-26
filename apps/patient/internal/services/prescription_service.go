f3ec96e67500b944d9db25d5c492666e12725779
sensh11 <alex.tursumetov@mail.ru>
2025-05-27 00:56:54 +0600

    feat(patient): Enhance prescription, allergy, and insurance services with CRUD operations and UUID integration.

--- apps/patient/internal/services/prescription_service.go
+++ apps/patient/internal/services/prescription_service.go
@@ -1,12 +1,64 @@
 package services
 
+import (
+	"github.com/google/uuid"
+	"patient/internal/dto"
+	"patient/internal/models"
+	"patient/internal/repositories"
+)
+
 type PrescriptionService interface {
+	Create(req *dto.CreatePrescriptionRequest) (*dto.PrescriptionResponse, error)
+	Delete(req *dto.DeletePrescriptionRequest) error
+	GetByPatient(id uuid.UUID) (*dto.PrescriptionResponses, error)
 }
 
 type prescriptionService struct {
+	repo repositories.PrescriptionRepository
 }
 
-func NewPrescriptionService() PrescriptionService {
-	return &prescriptionService{}
+func (p prescriptionService) Create(req *dto.CreatePrescriptionRequest) (*dto.PrescriptionResponse, error) {
+	prescription := &models.Prescription{
+		ID:         uuid.New(),
+		PatientID:  req.PatientID,
+		DoctorID:   req.DoctorID,
+		Medication: req.Medication,
+		Dosage:     req.Dosage,
+		ValidUntil: req.ValidUntil,
+	}
+
+	if err := p.repo.Create(prescription); err != nil {
+		return nil, err
+	}
+
+	return &dto.PrescriptionResponse{
+		ID:         prescription.ID,
+		PatientID:  prescription.PatientID,
+		DoctorID:   prescription.DoctorID,
+		Medication: prescription.Medication,
+		Dosage:     prescription.Dosage,
+		ValidUntil: prescription.ValidUntil,
+	}, nil
+}
+
+func (p prescriptionService) Delete(req *dto.DeletePrescriptionRequest) error {
+	return p.repo.Delete(req.PrescriptionID)
+
+}
+
+func (p prescriptionService) GetByPatient(id uuid.UUID) (*dto.PrescriptionResponses, error) {
+	prescriptions, err := p.repo.GetByPatient(id)
+
+	if err != nil {
+		return nil, err
+	}
+	responses := dto.NewPrescriptionResponses(prescriptions)
+	return responses, nil
+}
+
+func NewPrescriptionService(repo repositories.PrescriptionRepository) PrescriptionService {
+	return &prescriptionService{
+		repo: repo,
+	}
 
 }
