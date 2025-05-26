ebc306215bd7431129b5d2b1711bb9cba9e11437
Ruletk <damir.timergazin@gmail.com>
2025-05-27 03:39:56 +0500

    chore(appointment): Created function for converting model to response.

--- apps/appointment/internal/dto/response.go
+++ apps/appointment/internal/dto/response.go
@@ -20,3 +20,29 @@ type AppointmentListResponse struct {
 	TotalCount   int                   `json:"total_count"`
 	Appointments []AppointmentResponse `json:"appointments"`
 }
+
+func AppointmentResponseFromModel(a *model.Appointment) *AppointmentResponse {
+	return &AppointmentResponse{
+		ID:        a.ID.String(),
+		UserID:    a.UserID,
+		DoctorID:  a.DoctorID.String(),
+		Date:      a.Date.Format(time.RFC3339),
+		Status:    a.Status,
+		Notes:     a.Notes,
+		CreatedAt: a.CreatedAt,
+		UpdatedAt: a.UpdatedAt,
+	}
+}
+
+func AppointmentListResponseFromModel(appointments []*model.Appointment) *AppointmentListResponse {
+	response := &AppointmentListResponse{
+		TotalCount:   len(appointments),
+		Appointments: make([]AppointmentResponse, len(appointments)),
+	}
+
+	for i, appointment := range appointments {
+		response.Appointments[i] = *AppointmentResponseFromModel(appointment)
+	}
+
+	return response
+}
