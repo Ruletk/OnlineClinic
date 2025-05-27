0566496cb5dbcd9d667d18b5adea4cd65abc3896
Ruletk <damir.timergazin@gmail.com>
2025-05-27 15:55:07 +0500

    fix(patient): Commented unfinished grpc controller.

--- apps/patient/internal/controllers/grpc/patient_controller.go
+++ apps/patient/internal/controllers/grpc/patient_controller.go
@@ -1,42 +1,38 @@
 package grpcpackage
 
 import (
-	"context"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"patient/internal/models"
 	proto "patient/internal/proto/gen"
 	"patient/internal/services"
 )
 
 type PatientController struct {
 	proto.UnimplementedPatientServiceServer
-	service *services.PatientService
+	service services.PatientService
 }
 
-func NewPatientController(svc *interface{}) *PatientController {
+func NewPatientController(svc services.PatientService) *PatientController {
 	return &PatientController{service: svc}
 }
 
-func (c *PatientController) CreatePatient(ctx context.Context, req *proto.PatientRequest) (*proto.PatientResponse, error) {
-	patient := &models.Patient{
-		Height:    req.Height,
-		BloodType: req.BloodType,
-		Weight:    req.Weight,
-	}
-
-	if err := c.service.CreatePatient(patient); err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	return &proto.PatientResponse{
-		ID:         uint64(patient.ID),
-		BloodType:  patient.BloodType,
-		Height:     patient.Height,
-		Weight:     patient.Weight,
-		Allergies:  patient.Allergies,
-		Insurances: patient.Insurances,
-	}, nil
-}
+//func (c *PatientController) CreatePatient(req *proto.PatientRequest) (*proto.PatientResponse, error) {
+//	patient := &models.Patient{
+//		Height:    req.Height,
+//		BloodType: req.BloodType,
+//		Weight:    req.Weight,
+//	}
+//
+//	if err := c.service.CreatePatient(patient); err != nil {
+//		return nil, status.Error(codes.Internal, err.Error())
+//	}
+//
+//	return &proto.PatientResponse{
+//		ID:         uint64(patient.ID),
+//		BloodType:  patient.BloodType,
+//		Height:     patient.Height,
+//		Weight:     patient.Weight,
+//		Allergies:  patient.Allergies,
+//		Insurances: patient.Insurances,
+//	}, nil
+//}
 
 // Аналогичные реализации для GetPatient, UpdatePatient, DeletePatient
